f970036e8ba06ab328663f41f4166a5f9997f403
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2020-07-06 23:07:07 +0100

    change button

--- src/Control.js
+++ src/Control.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Control extends Component {
     render(props) {
         return (
-            <button classeName='btn' onClick={this.props.onClick}>
-                {this.props.BTN}
+            <button onClick={this.props.onClick}>
+                <h3>{this.props.BTN}</h3>
             </button>
         );
     }
