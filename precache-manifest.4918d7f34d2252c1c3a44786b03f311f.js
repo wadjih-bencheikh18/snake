d4902e3370ffaa785774d00715014873151ef4e4
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2020-07-06 22:55:18 +0100

    Updates

---
+++ precache-manifest.4918d7f34d2252c1c3a44786b03f311f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a9ffd0d29de7bf658d188917e2020e",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "b52381d17615780cf0c1",
+    "url": "/snake/static/css/main.89173d0d.chunk.css"
+  },
+  {
+    "revision": "a81e58ec132ce5af2453",
+    "url": "/snake/static/js/2.e3e96ae1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.e3e96ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52381d17615780cf0c1",
+    "url": "/snake/static/js/main.3637ef9d.chunk.js"
+  },
+  {
+    "revision": "330302f1424891b30bb2",
+    "url": "/snake/static/js/runtime-main.f5fb9ecf.js"
+  }
+]);
